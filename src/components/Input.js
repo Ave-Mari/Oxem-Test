47641b81b9facf5249482afa5d306d65372b5074
Ave-Mari <avemari98@gmail.com>
2022-10-06 03:22:13 +0300

    add class to input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -6,7 +6,7 @@ export default function Input( { placeholder, inputTextChange, inputRangeChange,
    <div className="block"> 
    <h3 className="gilroy-font">{inputTitle}</h3>
    <div className="input-wrapper">
-   <input type="text" placeholder={placeholder} onChange={inputTextChange}/>
+   <input className="text-input" type="text" placeholder={placeholder} onChange={inputTextChange}/>
     <input type="range" className="input-range" value={value} min={min} max={max} onChange={inputRangeChange}/>
     <p className="input-percent">{inputUnit}</p>
     </div>
