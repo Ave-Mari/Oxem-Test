7fc310640edee932a9fb7f479217b4f3a4348a2f
Ave-Mari <avemari98@gmail.com>
2022-10-17 23:05:48 +0300

    add erapper to inputs

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,10 +4,13 @@ export default function Input( { placeholder, inputTextChange, inputRangeChange,
   return (
     <>
    <div className="block"> 
+
    <h3 className="gilroy-font">{inputTitle}</h3>
    <div className="input-wrapper">
+   <div className="num-wrapper">
    <input className="text-input" type="text" placeholder={placeholder} onChange={inputTextChange}/>
-   <p className="input-percent">{inputUnit}</p>
+   <p className="input-unit">{inputUnit}</p>
+   </div> 
     <input type="range" className="input-range" value={value} min={min} max={max} onChange={inputRangeChange}/>   
     </div>
    </div>
